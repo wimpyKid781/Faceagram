26741a5eef686283411d3f79fe5372a7e2b07b95
wimpyKid781 <71785664+wimpyKid781@users.noreply.github.com>
2021-02-10 17:52:20 -0800

    Add files via upload

---
+++ Screens/fb.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text,View} from 'react-native'
+export default class Facebook extends React.Component{
+    render() {
+        return (
+            <View style = {{flex:1,justifyContent: 'center',alignItems: 'center'}}>
+                <Text>
+                    Facebook
+                </Text>
+            </View>
+        )
+    }
+}
